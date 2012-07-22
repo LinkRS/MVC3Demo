3888fbf4f9859e3b4310b7281e053390bb8e2c3d
Rich Selmon <LinkRTS@gmail.com>
2012-07-22 17:18:39 -0500

    Initial Code

---
+++ MVC3Demo/MVC3Demo/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC3Demo.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/ParameterPass
+
+        public ActionResult ParameterPass(string name)
+        {
+            ViewBag.Message = "Hello " + name;
+            return View(); 
+        }
+
+    }
+}
